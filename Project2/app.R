7ffbad5b2384b084585cfe11aa6f57941261ac5a
prakhar23 <prakhar.mishra@hotmail.com>
2019-10-15 16:26:00 -0400

    Added a donut chart
    Added a plotly donut chart that maps the distribution of cases by the age groups  that the perpetrators were in.

--- Project2/app.R
+++ Project2/app.R
@@ -12,16 +12,22 @@ library(httr)
 library(jsonlite)
 library(shiny)
 library(DT)
+library(leaflet)
+library(plotly)
 
+#Data Source - https://data.cityofnewyork.us/Public-Safety/NYPD-Arrests-Data-Historic-/8h9b-rp9u
 
 #Pulling the arrest data using an API
-
 path <- "https://data.cityofnewyork.us/resource/8h9b-rp9u.json?$limit=50000"
 request <- GET(url = path)
 request$status_code#Checking to ensure that a non-200 status code is not returned
 response <- content(request, as = "text", encoding = "UTF-8")
 arrest_data <- fromJSON(response) %>%  #Putting the pulled data into a data frame
   data.frame()
+
+#converting latitude and longitude values from characters to numeric 
+arrest_data$latitude <- as.numeric(as.character(arrest_data$latitude))
+arrest_data$longitude <- as.numeric(as.character(arrest_data$longitude))
 
 
 ui <- fluidPage(
@@ -45,7 +51,9 @@ ui <- fluidPage(
     
     tabsetPanel(
       
-      tabPanel("Data Table", br(), br(), uiOutput(outputId = "n"),br(), br(),DT::dataTableOutput("DataTable"))
+      tabPanel("Data Table", br(), br(), uiOutput(outputId = "n"),br(), br(),DT::dataTableOutput("DataTable")),
+      tabPanel("Donut Chart",br(), br(), plotlyOutput(outputId = "donut"))
+      # tabPanel("Map", br(), br(), br(), leafletOutput("map", height = "100%", width = "100%"))
     )
   )
   ) 
@@ -75,6 +83,27 @@ server <- function(input, output, session){
                   rownames = FALSE)
   )
   
+  #Server logic for bar chart plotting the bar chart with gender
+  
+  output$donut <- renderPlotly({
+    p <- arrest_data_subset() %>%
+      group_by(age_group) %>%
+      summarize(count = n()) %>%
+      plot_ly(labels = ~age_group, values = ~count) %>%
+      add_pie(hole = 0.6) %>%
+      layout(title = "Donut chart depicting percentage of cases by council district",  showlegend = T,
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  })
+  
+  
+  # #Rendering the map
+  # output$map <- renderLeaflet({
+  #   leaflet() %>%
+  #     addProviderTiles("OpenStreetMap.HOT") %>%
+  #     addCircleMarkers(data = arrest_data, lng = ~longitude, lat = ~latitude, radius = 1.5)
+  # })
+  # 
 }
 
 shinyApp(ui = ui, server = server)
